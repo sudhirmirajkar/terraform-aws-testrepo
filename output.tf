df6e254cce5c392063051d62bc30dc117fcf4fee
mirajkarsudhir <mirajkarsudhir@gmail.com>
2022-11-24 15:55:27 +0530

    moduleupdatev1

--- output.tf
+++ output.tf
@@ -1,7 +1,8 @@
+/*
 output "ec2arname" {
   value = aws_instance.lab5.arn
 }
-/*
+
 output "s3readonly" {
   value = aws_iam_role.s3role.name
 }
--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ module "TF-VPC-Lab" {
   name = "TF-VPC-Lab"
   cidr = "10.0.0.0/16"
 
-  azs             = [locals.useaz1, locals.useaz2]
+  azs             = ["locals.useaz1", "locals.useaz2"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
