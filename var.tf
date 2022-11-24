d3a9511a25dc6121e02477ad2b2a5e627a24ca46
mirajkarsudhir <mirajkarsudhir@gmail.com>
2022-11-24 16:14:32 +0530

    moduleupdatev1.3

--- var.tf
+++ var.tf
@@ -1,26 +1,26 @@
 variable "ec2amivar" {
-  type = string
-  //default = "ami-0b0dcb5067f052a63"
-default = "data.aws_ami.goldstandardami.id"
+  type    = string
+  default = "ami-0b0dcb5067f052a63"
+  //default = "data.aws_ami.goldstandardami.id"
 }
 
 variable "ec2typevar" {
-  type = string
+  type    = string
   default = "t2.micro"
 }
 
 variable "reagion" {
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
   description = "is the user does not mention the reagion he wants to build it will default to us-eat-1"
 }
 
 variable "sgcidrlist" {
-  type = list(string)
+  type    = list(string)
   default = ["0.0.0.0/0"]
 }
 
 variable "ec2name" {
-  type = list(string)
-  default = [ "lab" ]
+  type    = list(string)
+  default = ["lab"]
 }
